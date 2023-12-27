17cbaa66a3c6823b473d98c67e5ee7d0b932bc80
Ryan McCallister <ryan.mccallister.rm@gmail.com>
2023-12-27 13:40:15 -0600

    updates

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -13,7 +13,7 @@ const portNumber = ":3117"
 func main() {
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
 	http.HandleFunc("/", handlers.Home)
-	http.HandleFunc("/about", jsonHandlers.ServeJson)
+	http.HandleFunc("/content", jsonHandlers.ServeJson)
 	println("Starting application on port", portNumber)
 	http.ListenAndServe(portNumber, nil)
 }
